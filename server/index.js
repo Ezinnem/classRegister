e808d9f814981ac3f687a0ba437a9455145b4fd7
martina <you@example.com>
2023-06-13 04:24:09 +0100

    Created the Postgres database connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
+const cors = require("cors");
+const pool = require("./db")
 
 //MIDDLEWARE
 
 app.use(cors());
 app.use(express.json())
+
+//ROUTES
+
 
 app.listen(2000, ()=>{
     console.log("Server is running on port 2000")
